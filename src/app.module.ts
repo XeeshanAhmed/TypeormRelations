600ce5bad1034107a4ac50161e2245aeada1fe8c
HomePC <xeeshanahmad348@gmail.com>
2024-07-24 18:06:59 +0500

    Create order, customer and city resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { CustomerModule } from './customer/customer.module';
+import { CityModule } from './city/city.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, CustomerModule, CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
