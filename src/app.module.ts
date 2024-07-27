0a8cc35cadabaee7c49f76953842361aebeae3b1
HomePC <xeeshanahmad348@gmail.com>
2024-07-27 11:31:59 +0500

    Connect with databse using typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,26 @@ import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
 import { CustomerModule } from './customer/customer.module';
 import { CityModule } from './city/city.module';
-import { CustomerModule } from './customer/customer.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './order/entities/order.entity';
+import { Customer } from './customer/entities/customer.entity';
+import { City } from './city/entities/city.entity';
+import { join } from 'path';
 
 @Module({
-  imports: [OrderModule, CustomerModule, CityModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'YOUR_PASSWORD',      //replace this string with your actual postgres server password
+      database: 'YOUR_DATABASENAME',  //replace this string with your actual postgres database name
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+      logging:true,
+    }),
+    OrderModule, CustomerModule, CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
